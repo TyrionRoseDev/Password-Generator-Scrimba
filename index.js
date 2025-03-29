8f8427a39a18e90a775e53dd4075ee8f0f4d01ba
TyrionRoseDev <tyrionrosedev@gmail.com>
2025-03-29 12:30:21 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let getRandomPassword = document.getElementById("generate-password");
+let optionOne = document.getElementById("option-one");
+let optionTwo = document.getElementById("option-two");
+getRandomPassword.addEventListener("click", generate);
+
+function generateRandomCharacter() {
+  let random = Math.floor(Math.random() * characters.length);
+  return characters[random];
+}
+
+function generateRandomPassword() {
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    password += generateRandomCharacter();
+  }
+  return password;
+}
+function generate() {
+  optionOne.textContent = generateRandomPassword();
+  optionTwo.textContent = generateRandomPassword();
+}
+
+// TODO TDOGS GOALS
+// DARK MODE
+// WHAT CHARACTERS WANT TO USE ON TICKBOX
+// toggle length
+// STAR WARS THEME
