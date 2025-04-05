9aebb0d7415c24cb8a1f9d663f7739bec72acc4d
Tyrion Meredith <tyrionrosedev@gmail.com>
2025-04-05 13:25:36 +0100

    finalised design

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-console.log("gooch");
-
-const characters = [
+const letters = [
   "A",
   "B",
   "C",
@@ -53,16 +51,10 @@ const characters = [
   "x",
   "y",
   "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
+];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = [
   "~",
   "`",
   "!",
@@ -97,9 +89,47 @@ const characters = [
 let getRandomPassword = document.getElementById("generate-password");
 let optionOne = document.getElementById("option-one");
 let optionTwo = document.getElementById("option-two");
+
+optionOne.addEventListener("click", function () {
+  navigator.clipboard.writeText(optionOne.textContent);
+  optionOne.style.transform = "scale(0.95)";
+  setTimeout(() => {
+    optionOne.style.transform = "scale(1)";
+  }, 100);
+});
+optionTwo.addEventListener("click", function () {
+  navigator.clipboard.writeText(optionTwo.textContent);
+  optionTwo.style.transform = "scale(0.95)";
+  setTimeout(() => {
+    optionTwo.style.transform = "scale(1)";
+  }, 100);
+});
+
 getRandomPassword.addEventListener("click", generate);
 
+let toggleNumbersEl = document.getElementById("toggle-numbers");
+let toggleSpecialCharsEl = document.getElementById("toggle-special-characters");
+
+let includeSpecial = false;
+let includeNumbers = false;
+
+toggleNumbersEl.addEventListener("change", (e) => {
+  includeNumbers = !includeNumbers;
+});
+
+toggleSpecialCharsEl.addEventListener("change", (e) => {
+  includeSpecial = !includeSpecial;
+});
+
 function generateRandomCharacter() {
+  let characters = [];
+  characters.push(...letters);
+  if (includeNumbers) {
+    characters.push(...numbers);
+  }
+  if (includeSpecial) {
+    characters.push(...special);
+  }
   let random = Math.floor(Math.random() * characters.length);
   return characters[random];
 }
@@ -111,12 +141,14 @@ function generateRandomPassword() {
   }
   return password;
 }
-function generate() {
-  optionOne.textContent = generateRandomPassword();
-  optionTwo.textContent = generateRandomPassword();
-}
 const lightsaber = document.getElementById("lightsaber");
 console.log(lightsaber);
 lightsaber.addEventListener("click", function () {
   this.classList.toggle("active");
 });
+
+function generate() {
+  optionOne.textContent = generateRandomPassword();
+  optionTwo.textContent = generateRandomPassword();
+  lightsaber.classList.add("active");
+}
